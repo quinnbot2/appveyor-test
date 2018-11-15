92db76ce1b6cf77aa5d05e4454699fd7de9a5632
quinnbot2 <quinnbot@gmail.com>
2018-11-15 10:37:02 -0800

    Testing appveyor against a library + program combo

---
+++ LibTest/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTest
+{
+    public class ParserClass
+    {
+        string filename;
+
+        public void Parse(string inFile)
+        {
+            // do some shit 
+
+            Console.WriteLine("Success!");
+        }
+    }
+}
---
+++ ParseTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibTest;
+
+namespace ParseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParserClass foo = new ParserClass();
+
+            foo.Parse("parse this file"); 
+
+            Console.ReadLine();
+        }
+    }
+}
